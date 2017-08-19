234e2843b9d2a41e64b422d0a5e74990a1d5d5f0
Brittany <brithamilton1@gmail.com>
2017-08-19 13:53:25 -0400

    updated factory

--- public/event.factory.js
+++ public/event.factory.js
@@ -6,12 +6,10 @@ app.factory('EventFactory', function($http){
     getAnswers: getAnswers,
   };
   function getAnswers (category){
-    category = category || 'performingArts';
+    category = category;
     if(postData){
-
       return Promise.resolve(postData);
     }
-  
 
     return $http.get(`api-event?app_key=cMG6K3XWNbCQbjfj&category=${category}&location=Detroit&date=Future`)
       .then(function(response){
