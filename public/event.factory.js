f758cd52024db70b44498b86a7a43b429f3c68c6
Brittany <brithamilton1@gmail.com>
2017-08-19 16:54:54 -0400

    route working, need to fix questions

--- public/event.factory.js
+++ public/event.factory.js
@@ -1,21 +1,27 @@
 var app = angular.module("myApp");
 app.factory('EventFactory', function($http){
   var postData;
+  var currentCategory;
+
 
   return {
     getAnswers: getAnswers,
+    saveCategory: saveCategory
   };
-  function getAnswers (category){
-    category = category;
-    if(postData){
-      return Promise.resolve(postData);
-    }
 
-    return $http.get(`api-event?app_key=cMG6K3XWNbCQbjfj&category=${category}&location=Detroit&date=Future`)
+  function saveCategory(category){
+    currentCategory = category;
+  }
+
+  function getAnswers (){
+    // if(postData){
+    //   return Promise.resolve(postData);
+    // }
+     console.log('in get answers');
+    return $http.get(`api-event?app_key=cMG6K3XWNbCQbjfj&category=${currentCategory}&location=Detroit&date=Future`)
       .then(function(response){
         postData = response.data.events.event;
-        console.log(postData);
         return postData;
-     })
+     });
   }
   });
--- public/questionsController.js
+++ public/questionsController.js
@@ -1,31 +1,26 @@
 var app = angular.module('myApp');
-
-  app.controller('QuestionCtrl', function($scope){
-    $scope.randomizer = questions[Math.floor(Math.random()*questions.length)];
-    $scope.pick = function(){
-      $scope.randomizer = questions[Math.floor(Math.random()*questions.length)];
-    };
-  });
-
-  var  questions = [
-  {category: "performingArts", question: "Chaga is one of the weirdest mushrooms you may ever see."},
-  {category: "performingArts", question: "We all know interspecies romance is weird."},
-  {category: "performingArts", question: "Im glad apples don't scream when you bite into them."},
-  {category: "performingArts", question: "A Freudian slip is when you say one thing but mean your mother."},
-  {category: "performingArts", question: "Some mornings, it's just not worth chewing through the leather straps"},
-  {category: "performingArts", question: "Life may have no meaning. Or even worse, it may have a meaning of which I disapprove."},
-  {category: "sports", question: "Chaga is one of the weirdest mushrooms you may ever see."},
-  {category: "sports", question: "We all know interspecies romance is weird."},
-  {category: "sports", question: "Im glad apples don't scream when you bite into them."},
-  {category: "sports", question: "A Freudian slip is when you say one thing but mean your mother."},
-  {category: "sports", question: "Some mornings, it's just not worth chewing through the leather straps"},
-  {category: "sports", question: "Life may have no meaning. Or even worse, it may have a meaning of which I disapprove."},
-  {category: "music", question: "Chaga is one of the weirdest mushrooms you may ever see."},
-  {category: "music", question: "We all know interspecies romance is weird."},
-  {category: "music", question: "Im glad apples don't scream when you bite into them."},
-  {category: "music", question: "A Freudian slip is when you say one thing but mean your mother."},
-  {category: "music", question: "Some mornings, it's just not worth chewing through the leather straps"},
-  {category: "music", question: "Life may have no meaning. Or even worse, it may have a meaning of which I disapprove."}
+var  questions = [
+  {category: "performingArts", question: "Do you like performing arts?"},
+  {category: "performingArts", question: "Want to see a broadway show in Detroit?"},
+  {category: "performingArts", question: "Ready to seeing performing arts?"},
+  {category: "sports", question: "Do you want to cheer on your favorite teams?"},
+  {category: "sports", question: "Do you want to meet your favorite mascot?"},
+  {category: "sports", question: "Do you like sports?"},
+  {category: "music", question: "Do you like music?"},
+  {category: "music", question: "Do you want to crowdsurf?"},
+  {category: "music", question: "Ready to see your favorite band?"},
 ];
+
+app.controller('QuestionCtrl', function($scope, $location, $timeout, EventFactory){
+    $scope.randomizer = questions[Math.floor(Math.random()*questions.length)];
+    $scope.save = function(category){
+      EventFactory.saveCategory(category);
+      $timeout(function(){$location.path("/category")});
+    }
+    $scope.pick = function(){
+      $scope.randomizer = questions[Math.floor(Math.random()*questions.length)]
+    }
+});
+
 
 
