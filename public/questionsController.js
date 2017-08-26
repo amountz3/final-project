ec20c3fe58a19d519ea276d227815d304fcc13d6
Brittany <brithamilton1@gmail.com>
2017-08-26 11:26:46 -0400

    show more button aand logo

--- public/questionsController.js
+++ public/questionsController.js
@@ -12,10 +12,10 @@ var  questions = [
 ];
 
 app.controller('QuestionCtrl', function($scope, $location, $timeout, EventFactory){
-  // $scope.showLogo = true; 
-  // $timeout(function(){
-  //   $scope.showLogo = false;
-  // },2000)
+  $scope.showLogo = true; 
+  $timeout(function(){
+    $scope.showLogo = false;
+  },2000)
     $scope.randomizer = questions[Math.floor(Math.random()*questions.length)];
     $scope.save = function(category){
       EventFactory.saveCategory(category);
